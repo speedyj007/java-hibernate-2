94eff4b46ae839aac0c2dcebfaab748f11ba03fe
Prabeer <Prabeer@Prabeer-PC>
2019-04-01 14:52:02 +0530

    This program is about connecting mysql dataabse with hiberate 5.1

---
+++ src/javaandhibernate_app/members.java
@@ -0,0 +1,63 @@
+
+package javaandhibernate_app;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class members {
+    @Id
+    private int id;
+    private String firstname, lastname, email, mobile, password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
